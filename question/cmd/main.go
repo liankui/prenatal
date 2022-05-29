ec5585df2e42b64e83f33bbdc3acb324b11a8967
liankui <yuekewin@gmail.com>
2022-05-29 18:46:01 +0800

    use kit command line create service question

---
+++ question/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "github.com/liankui/prenatal-server/question/cmd/service"
+
+func main() {
+	service.Run()
+}
---
+++ question/pkg/service/service.go
@@ -0,0 +1,30 @@
+package service
+
+import "context"
+
+// QuestionService describes the service.
+type QuestionService interface {
+	// Add your methods here
+	Foo(ctx context.Context, s string) (rs string, err error)
+}
+
+type basicQuestionService struct{}
+
+func (b *basicQuestionService) Foo(ctx context.Context, s string) (rs string, err error) {
+	// TODO implement the business logic of Foo
+	return rs, err
+}
+
+// NewBasicQuestionService returns a naive, stateless implementation of QuestionService.
+func NewBasicQuestionService() QuestionService {
+	return &basicQuestionService{}
+}
+
+// New returns a QuestionService with all of the expected middleware wired in.
+func New(middleware []Middleware) QuestionService {
+	var svc QuestionService = NewBasicQuestionService()
+	for _, m := range middleware {
+		svc = m(svc)
+	}
+	return svc
+}
